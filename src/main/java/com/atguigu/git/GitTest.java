4d9d0ec5575ab07f3e71ddca7f0f516e0eeab6c0
lcjxwz <lcj681951@163.com>
2022-08-18 15:59:42 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hot-fix text!");
         System.out.println("push text!");
         System.out.println("pull text!");
+        System.out.println("gitee text!");
 
     }
 }
